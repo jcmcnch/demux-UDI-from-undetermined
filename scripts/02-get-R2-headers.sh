a04dfbb9a79fcae2c37e9781c2edd848c3fac25e
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-09-19 15:54:57 -0700

    Update repo

--- scripts/02-get-R2-headers.sh
+++ scripts/02-get-R2-headers.sh
@@ -1,2 +1,18 @@
 #!/bin/bash -i
-for item in 220919_demultiplexed/*R2.fastq ; do filestem=`basename $item -R2.fastq` ; grep "^@" $item ; done
+
+#make output dirs
+mkdir -p i5-headers i5-bc2sample
+
+for item in 220919_demultiplexed/*R2.fastq ; do 
+	
+	#get sample name
+	filestem=`basename $item -R2.fastq`
+
+	#pull out illumina headers
+	grep "^@" $item > i5-headers/$filestem.headers.txt
+
+	#make new individual sample-to-barcode.tsv files for second round of illumina-utils
+	#this is just a culling step
+	grep $filestem i7index_pool1.noheader.tsv > i5-bc2sample/$filestem.bc2sample.tsv
+
+done
