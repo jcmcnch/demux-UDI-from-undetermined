dffefba4d1f84e70e3c32b5e6187aa7ec6b59cbd
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-09-20 10:35:13 -0700

    Update repo

--- scripts/02-get-R2-headers.sh
+++ scripts/02-get-R2-headers.sh
@@ -9,9 +9,9 @@ for item in 220919_demultiplexed/*R2.fastq ; do
 	filestem=`basename $item -R2.fastq`
 
 	#pull out illumina headers, stripping "@" character so filterbyname.sh will work
-	grep "^@" $item | sed 's/@//' > i5-headers/$filestem.headers.txt
+	#grep "^@" $item | sed 's/@//' > i5-headers/$filestem.headers.txt
 
 	#make new individual sample-to-barcode.tsv files for second round of illumina-utils
-	grep $filestem i7index_pool1.noheader.tsv > i5-bc2sample/$filestem.bc2sample.tsv
+	grep $filestem i5index_v1.5_pool1.tsv > i5-bc2sample/$filestem.bc2sample.tsv
 
 done
--- scripts/03-get-i5-index-from-headers.sh
+++ scripts/03-get-i5-index-from-headers.sh
@@ -7,8 +7,9 @@ for item in 220919_demultiplexed/*R2.fastq ; do
 
         #get sample name
         filestem=`basename $item -R2.fastq`
+	I2=Undetermined_S0_L001_I2_001.fastq.gz
 
         #pull out illumina headers
-        filterbyname.sh in=$item names=i5-headers/$filestem.headers.txt include=t out=${outdir}-I2.fastq
+        filterbyname.sh in=$I2 names=i5-headers/$filestem.headers.txt include=t out=$outdir/${filestem}-I2.fastq
 
 done
